0563c9fc12d73528b9e9dc6616e01c2dc6928195
LucaLeonardelli <luca.leonardelli@mat.tn.it>
2026-04-09 19:46:04 +0000

    feat: aggiunti form di inserimento e svuotamento catalogo
    Implementati endpoint /new e /clear nel Controller, validazione su anno e categoria in form.html e aggiunti i pulsanti in index.html

--- catalogo-vini/src/main/java/com/lucaleonardelli/catalogo_vini/controllers/VinoController.java
+++ catalogo-vini/src/main/java/com/lucaleonardelli/catalogo_vini/controllers/VinoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.lucaleonardelli.catalogo_vini.domain.Vino;
 import com.lucaleonardelli.catalogo_vini.repositories.VinoRepository;
@@ -16,10 +17,30 @@ public class VinoController {
     @Autowired
     private VinoRepository vinoRepository;
 
+    ////////////////// HOMEPAGE
     @GetMapping("/")
     public String home(Model model) {
         List<Vino> vini = vinoRepository.findAll();
         model.addAttribute("listaVini", vini);
         return "index";
+    }
+
+    ////////////////// FORM CREAZIONE
+    @GetMapping("/new")
+    public String mostraFormAggiunta(Model model) {
+        model.addAttribute("vino", new Vino());
+        return "form";
+    }
+    @PostMapping("/new")
+    public String salvaVino(Vino vino) {
+        vinoRepository.save(vino);
+        return "redirect:/";
+    }
+
+    ////////////////// PAGINA ELIMINAZIONE
+    @GetMapping("/clear")
+    public String svuotaCatalogo() {
+        vinoRepository.deleteAll();
+        return "redirect:/";
     }
 }
